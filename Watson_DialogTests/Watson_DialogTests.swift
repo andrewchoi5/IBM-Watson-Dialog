a9005a06d8684a4b523f339b8b1ba43d8f2b7c07
Andrew Choi <achoi@ca.ibm.com>
2016-03-07 19:36:39 -0500

    Initial Commit

---
+++ Watson_DialogTests/Watson_DialogTests.swift
@@ -0,0 +1,36 @@
+//
+//  Watson_DialogTests.swift
+//  Watson_DialogTests
+//
+//  Created by Andrew Choi on 2016-03-07.
+//  Copyright © 2016 Archetapp. All rights reserved.
+//
+
+import XCTest
+@testable import Watson_Dialog
+
+class Watson_DialogTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
